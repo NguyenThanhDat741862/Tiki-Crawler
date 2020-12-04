a619c8e876ffef855983b01d7c5f573a9d2e6daa
nguyenthanhdat741862 <nguyenthanhdat741862@gmail.com>
2020-12-04 18:37:58 +0700

    Crawl category URL list

---
+++ crawlCategoryURL.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+const puppeteer = require('puppeteer')
+
+const url = 'https://tiki.vn/'
+
+async function run () {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(url)
+  
+  let links = []
+
+  const mainNavigationItems = await page.$$('[data-view-id="main_navigation_item"]')
+  
+  const mainNavigationURLs = await page.$$eval(
+    '[data-view-id="main_navigation_item"] > a',
+    links => links.map(i => i.getAttribute('href'))
+  )
+
+  links = [...links, ...mainNavigationURLs]
+
+  for (let i = 1; i <= mainNavigationItems.length; i++) {
+    await page.hover(`[data-view-id="main_navigation_item"]:nth-child(${i})`)
+
+    await page.waitFor(5000)
+
+    const subNavigationURLs = await page.$$eval(
+      '[data-view-id="main_navigation_sub_item"] a',
+      links => links.map(i => i.getAttribute('href'))
+    )
+
+    links = [...links, ...subNavigationURLs]
+  }
+
+  browser.close()
+
+  fs.writeFileSync('./data/categoryURL.txt', links.reduce((res, i) => res + i + '\n', ''))
+}
+
+run()
