eaea2e1fbc3643cec31147b0b888985caa5f5793
nguyenthanhdat741862 <nguyenthanhdat741862@gmail.com>
2021-01-26 00:30:07 +0700

    Fix comma in field csv

--- converter/ProductJsonToCsv.js
+++ converter/ProductJsonToCsv.js
@@ -6,6 +6,10 @@ const parser = JSONStream.parse([{emitKey: true}])
 
 stream.pipe(parser)
 
+function toBase64 (str) {
+  return Buffer.from(str).toString('base64')
+}
+
 function main () {
   try {
     const writeStream = fs.createWriteStream('./data/csv/product.csv', { flags: 'a' })
@@ -14,7 +18,7 @@ function main () {
     parser.on('data', function (data) {
       const { id, name, price, description, specifications, productset_group_name } = data.value
 
-      writeStream.write(`${id},${JSON.stringify(name)},${price},${JSON.stringify(description)},${JSON.stringify(specifications)},${JSON.stringify(productset_group_name)}\n`)
+      writeStream.write(`${id},${JSON.stringify(name)},${price},${toBase64(description)},"${JSON.stringify(specifications).replace(/"/g, '""')}",${JSON.stringify(productset_group_name)}\n`)
     })    
 
     parser.on('footer', function () {
