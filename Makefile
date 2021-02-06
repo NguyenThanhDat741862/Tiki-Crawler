286f3737fa50c783804993376e7cdd5645045434
nguyenthanhdat741862 <nguyenthanhdat741862@gmail.com>
2021-02-06 19:18:12 +0700

    Done ( wait for test)

---
+++ Makefile
@@ -0,0 +1,8 @@
+reset-project: 
+	rm ./tmp/* || \
+	rm ./data/categoryURL/* || \
+	rm ./data/productID/* || \
+	rm ./data/product/output/* || \
+	rm ./data/product/error/* || \
+	rm ./data/review/output/* || \
+	rm ./data/review/error/*
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const { exec, execSync, spawn } = require('child_process')
+const { execSync } = require('child_process')
 const { chunkArray } = require('./helper')
 
 process.on('uncaughtException', function (e) {
@@ -8,44 +8,44 @@ process.on('uncaughtException', function (e) {
 })
 
 // // Crawl Category URL
-// console.log(`${Date.now()} - Crawl category URL`)
+console.log(`${Date.now()} - Crawl category URL`)
 
-// try {
-//   execSync('node ./modules/crawlCategoryURL/crawlCategoryURL.js', { stdio: 'inherit' })
-// } catch (e) {
-//   console.error(`${Date.now()} - Error: ${e}`)
-//   process.exit(1)
-// }
+try {
+  execSync('node ./modules/crawlCategoryURL/crawlCategoryURL.js', { stdio: 'inherit' })
+} catch (e) {
+  console.error(`${Date.now()} - Error: ${e}`)
+  process.exit(1)
+}
 
 
 // // Generate crawled Category Code
-// console.log(`${Date.now()} - Split category code into chunks`)
+console.log(`${Date.now()} - Split category code into chunks`)
 
-// const categoryUrls = JSON.parse(fs.readFileSync('./data/categoryURL/categoryURL.json', 'utf8'))
-// const crawledCategoryCode = Array.from(new Set(categoryUrls.map(i => i.url.match(/c{1}\d{4,5}/g) && i.url.match(/c{1}\d{4,5}/g)[0].replace('c', '')).filter(i => i)))
+const categoryUrls = JSON.parse(fs.readFileSync('./data/categoryURL/categoryURL.json', 'utf8'))
+const crawledCategoryCode = Array.from(new Set(categoryUrls.map(i => i.url.match(/c{1}\d{4,5}/g) && i.url.match(/c{1}\d{4,5}/g)[0].replace('c', '')).filter(i => i)))
 
-// chunkArray(crawledCategoryCode, 50).forEach((chunk, i) => {
-//   try {
-//     fs.writeFileSync(`./tmp/categoryURL-${Date.now()}.json`, `[${chunk.toString()}]`)
-//   } catch (e) {
-//     console.error(`${Date.now()} - Error: ${e}`)
-//   }
-// })
+chunkArray(crawledCategoryCode, 50).forEach((chunk, i) => {
+  try {
+    fs.writeFileSync(`./tmp/categoryURL-${Date.now()}.json`, `[${chunk.toString()}]`)
+  } catch (e) {
+    console.error(`${Date.now()} - Error: ${e}`)
+  }
+})
 
 
 // // Crawl Prodict ID
-// console.log(`${Date.now()} - Crawl product ID`)
+console.log(`${Date.now()} - Crawl product ID`)
 
-// const categoryCodeFiles = fs.readdirSync('./tmp')
+const categoryCodeFiles = fs.readdirSync('./tmp')
 
-// categoryCodeFiles.forEach(file => {
-//   try {
-//     execSync(`node ./modules/crawlProductID/crawlProductID.js ${file}`, { stdio: 'inherit' })
-//   } catch (e) {
-//     console.error(`${Date.now()} - Error: ${e}`)
-//     process.exit(1)
-//   }
-// })
+categoryCodeFiles.forEach(file => {
+  try {
+    execSync(`node ./modules/crawlProductID/crawlProductID.js ${file}`, { stdio: 'inherit' })
+  } catch (e) {
+    console.error(`${Date.now()} - Error: ${e}`)
+    process.exit(1)
+  }
+})
 
 
 // Crawl Product, Review
